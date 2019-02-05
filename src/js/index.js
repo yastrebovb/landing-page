27698f73e2f71c60f16decc6367e362f9bf859d6
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-02-05 23:07:52 +0300

    major updates, slider component added

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 import '../scss/main.scss'
+
+import Glide from '@glidejs/glide'
+
+new Glide('.glide', {
+  type: 'carousel',
+  perView: 3,
+  focusAt: 'center',
+  gap: '32px',
+  unlimited: true,
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+    480: {
+      perView: 1
+    }
+  }
+}).mount()
