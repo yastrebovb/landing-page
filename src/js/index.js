7855908529671c3d131ee1a3653dd861aa23c1e6
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-02-08 00:33:09 +0300

    component slider in features

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,27 @@ import '../scss/main.scss'
 
 import Glide from '@glidejs/glide'
 
-new Glide('.glide', {
+new Glide('.glide--reviews', {
   type: 'carousel',
   perView: 3,
   focusAt: 'center',
   gap: '32px',
+  unlimited: true,
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+    480: {
+      perView: 1
+    }
+  }
+}).mount()
+
+new Glide('.glide--features', {
+  type: 'carousel',
+  perView: 4,
+  focusAt: 'center',
+  gap: '56px',
   unlimited: true,
   breakpoints: {
     800: {
