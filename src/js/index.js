6d60ba0e1a401ef1a2668b9678d01e65363c7c49
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-02-28 22:33:49 +0300

    aos library added and js file split into small modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,8 @@
 import '../scss/main.scss'
 
-import Glide from '@glidejs/glide'
+import './slider'
 
-new Glide('.glide--reviews', {
-  type: 'carousel',
-  perView: 3,
-  focusAt: 'center',
-  gap: '32px',
-  perTouch: false,
-  unlimited: true,
-  breakpoints: {
-    800: {
-      perView: 2
-    },
-    480: {
-      perView: 1
-    }
-  }
-}).mount()
-
-new Glide('.glide--features', {
-  type: 'carousel',
-  perView: 4,
-  focusAt: 'center',
-  gap: '56px',
-  perTouch: false,
-  unlimited: true,
-  breakpoints: {
-    800: {
-      perView: 2
-    },
-    480: {
-      perView: 1
-    }
-  }
-}).mount()
+import './animate'
 
 document.querySelectorAll('form').forEach(form => {
   form.addEventListener('submit', e => {
---
+++ src/js/slider.js
@@ -0,0 +1,35 @@
+import Glide from '@glidejs/glide'
+
+new Glide('.glide--reviews', {
+  type: 'carousel',
+  perView: 3,
+  focusAt: 'center',
+  gap: '32px',
+  perTouch: false,
+  unlimited: true,
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+    480: {
+      perView: 1
+    }
+  }
+}).mount()
+
+new Glide('.glide--features', {
+  type: 'carousel',
+  perView: 4,
+  focusAt: 'center',
+  gap: '56px',
+  perTouch: false,
+  unlimited: true,
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+    480: {
+      perView: 1
+    }
+  }
+}).mount()
